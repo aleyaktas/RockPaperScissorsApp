eae248e67609eb400268cc75df5586083734fba6
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-07-08 23:33:00 +0300

    Add calculation and display of game result

--- RockPaperScissorsApp/Game.swift
+++ RockPaperScissorsApp/Game.swift
@@ -14,7 +14,8 @@ class Game: UIViewController {
     var timer: Timer?
     var runCount = 3
     var score = 0
-
+    
+    var computerChoose:String = ""
     var selectedOption: String = ""
     
     @IBOutlet weak var chooseButton: UIButton!
@@ -25,6 +26,9 @@ class Game: UIViewController {
     @IBOutlet weak var paper: UIButton!
     @IBOutlet weak var scissor: UIButton!
 
+    
+    @IBOutlet weak var userChooseImg: UIImageView!
+    @IBOutlet weak var computerChooseImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +44,31 @@ class Game: UIViewController {
         case scissor = "Scissor"
     }
 
+    func randomSelection() -> String {
+        let options = ["Rock", "Paper", "Scissor"]
+        let randomIndex = Int.random(in: 0..<3)
+        return options[randomIndex]
+    }
+
+    func showMessage (with message: String) {
+        counter.text = message
+    }
+
+    func checkStatus (userChoice: String)   {
+
+        computerChoose = randomSelection()
+        if let choose = Status(rawValue: userChoice) {
+            print("👉 Computer choose: \(computerChoose), Your choose: \(userChoice)")
+            switch choose {
+            case .rock:
+                computerChoose == "Scissor" ? showMessage(with: "Win") : (computerChoose == "Rock" ? showMessage(with: "Draw") : showMessage(with: "Lose"))
+            case .paper:
+                computerChoose == "Rock" ? showMessage(with: "Win") : (computerChoose == "Paper" ? showMessage(with: "Draw") : showMessage(with: "Lose"))
+            case .scissor:
+                computerChoose == "Paper" ? showMessage(with: "Win") : (computerChoose == "Scissor" ? showMessage(with: "Draw") : showMessage(with: "Lose"))
+            }
+        }
+    }
 
     @IBAction func toggle(_ sender: Any) {
         optionsVisible.toggle()
@@ -50,16 +79,33 @@ class Game: UIViewController {
     
     @IBAction func selectButton(_ sender: UIButton) {
         runCount = 3
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeControl), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+
+        counter.text = String(runCount)
+        switch sender.tag {
+           case 1:
+            selectedOption = "Rock"
+           case 2:
+            selectedOption = "Paper"
+           case 3:
+            selectedOption = "Scissor"
+           default:
+               break
+           }
+        
     }
     
-    @objc func timeControl() {
-        print("Timer fired!")
+    @objc func fireTimer() {
         runCount -= 1
         counter.text = String(runCount)
-        print(runCount)
 
         if runCount == 0 {
+            checkStatus(userChoice: selectedOption)
+            print(computerChoose)
+            let userImg = UIImage(named: selectedOption + "User")
+            let computerImg = UIImage(named: computerChoose + "Computer")
+            userChooseImg.image = userImg
+            computerChooseImg.image = computerImg
             timer?.invalidate()
         }
     }
