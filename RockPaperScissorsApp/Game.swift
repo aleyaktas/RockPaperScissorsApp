a84a0cdfa3e66640bada8302ddcd93b91ed70c6d
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-07-08 23:08:17 +0300

    Add time control function for counter

--- RockPaperScissorsApp/Game.swift
+++ RockPaperScissorsApp/Game.swift
@@ -11,33 +11,56 @@ class Game: UIViewController {
     
     private var optionsVisible = false
 
-    @IBOutlet weak var buttonStack: UIStackView!
+    var timer: Timer?
+    var runCount = 3
+    var score = 0
+
+    var selectedOption: String = ""
     
     @IBOutlet weak var chooseButton: UIButton!
+    
+    @IBOutlet weak var counter: UILabel!
     
     @IBOutlet weak var rock: UIButton!
     @IBOutlet weak var paper: UIButton!
     @IBOutlet weak var scissor: UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
         rock.isHidden = !optionsVisible
         paper.isHidden = !optionsVisible
-
         scissor.isHidden = !optionsVisible
-        buttonStack.layer.cornerRadius = 16
-
     }
     
+    enum Status: String {
+        case rock = "Rock"
+        case paper = "Paper"
+        case scissor = "Scissor"
+    }
+
 
     @IBAction func toggle(_ sender: Any) {
         optionsVisible.toggle()
         rock.isHidden = !optionsVisible
         paper.isHidden = !optionsVisible
-
         scissor.isHidden = !optionsVisible
-
     }
     
+    @IBAction func selectButton(_ sender: UIButton) {
+        runCount = 3
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeControl), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timeControl() {
+        print("Timer fired!")
+        runCount -= 1
+        counter.text = String(runCount)
+        print(runCount)
+
+        if runCount == 0 {
+            timer?.invalidate()
+        }
+    }
 }
